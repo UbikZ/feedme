17414a8b3255440bbc8b7f3ad45f5874eb40683c
Gabriel Malet <UbikZ@users.noreply.github.com>
2014-08-12 16:55:58 +0200

    Add some "todo" in Feedme\Logger

--- libraries/Feedme/Logger/Factory.php
+++ libraries/Feedme/Logger/Factory.php
@@ -8,6 +8,10 @@ class Factory
 {
     public static function getLogger($path)
     {
+        /*
+         * We setup a default adapter for now
+         * todo: improve logger with other adapters
+         */
         $adapter = "File";
         $prefixAdapter = "Feedme\\Logger\\Adapters\\";
         if (!class_exists($className = $prefixAdapter . $adapter)) {
